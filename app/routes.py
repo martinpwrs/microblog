3c8722a7f0aed93b473a8c662e159291c1b2562f
Martin Powers <martinpwrs@gmail.com>
2019-04-14 20:50:00 +0000

    switch to index template

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
+from flask import render_template
 from app import app
-@app.route('/')
-@app.route('/index')
+
+@app.route("/")
+@app.route("/index")
 def index():
-    return "Hello, World!"
+    user = {"username": "Martin"}
+    return render_template("index.html", title="Home", user=user)
